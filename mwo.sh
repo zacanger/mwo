94cb097d77bc2478e13bb456aa64525632e6e06f
zacanger <zac@zacanger.com>
2017-07-31 19:39:44 -0600

    What am I doing wrong?

--- mwo.sh
+++ mwo.sh
@@ -1,8 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+thing="$@"
+
+if [ -f "$HOME/.bashrc" ] ; then
+  echo "yes"
+  . "$HOME/.bashrc"
+  shopt -s expand_aliases
+fi
 
 echo '뭐?'
 
+if [ -d "$thing" ] || [ -f "$thing" ] ; then
+  file $thing
+elif [ -n "`which $thing`" ] ; then
+  which $thing
+elif [ -n "`type $thing`" ] ; then
+  type -t $thing
+fi
+
+# order: type then which, or which then type?
 # file
 # type
 # which
-# aproppos?
+# apropos?
